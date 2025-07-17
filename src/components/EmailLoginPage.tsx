177387f2e1540f0f0816348461d1c4aa3983489d
Laurent Direr <4263535+ldirer@users.noreply.github.com>
2025-07-17 18:39:03 +0200

    prepare frontend for claude

--- src/components/EmailLoginPage.tsx
+++ src/components/EmailLoginPage.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
 import { useURLParams } from '../hooks/useURLParams.ts'
 
-const useEmailLoginToken = (): TaggedStoreId[] => {
+const useEmailLoginToken = () => {
   const params = useURLParams()
   return params.token
 }
 const EmailLoginPage = () => {
+  // 'magic link' login: extract the token from url, use it to log in with the server and redirect to the home page
   const token = useEmailLoginToken()
   useEffect(() => {
     if (token !== undefined) {
