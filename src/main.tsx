ed0c116e56069b914ec747c799186353ea392684
Laurent Direr <4263535+ldirer@users.noreply.github.com>
2025-07-18 17:37:07 +0200

    tools: add react strict mode back

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './Root.js'
 
 const rootEl = document.getElementById('react-app')
 if (!rootEl) throw new Error('Missing #react-app')
-ReactDOM.createRoot(rootEl).render(<App />)
-
-// ReactDOM.createRoot(document.getElementById('react-app')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
