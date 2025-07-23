484139ae6347fb9f3ffbb07b56af4ed9d198c191
Laurent Direr <4263535+ldirer@users.noreply.github.com>
2025-07-23 19:29:30 +0200

    fix _tiny_ issue with backend sqlite3 not working

--- flake.nix
+++ flake.nix
@@ -20,10 +20,22 @@
         default = pkgs.mkShell {
           packages = with pkgs; [
             deno
+            nodejs_24
             bun
             flyctl
             lefthook
           ];
+
+        shellHook = ''
+          # I find this immensely lame but I didn't find a better solution
+          # this is all for the sqlite JS lib to find libstdc++.so.
+          # changing LD_LIBRARY_PATH in the global env breaks things (ex: kitty).
+          mkdir -p .bin
+          echo '#!/bin/sh' > .bin/bun
+          echo 'LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib exec ${pkgs.bun}/bin/bun "$@"' >> .bin/bun
+          chmod +x .bin/bun
+          export PATH="$(pwd)/.bin:$PATH"
+        '';
         };
       });
     };
